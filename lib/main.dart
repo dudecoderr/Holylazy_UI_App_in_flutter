81e2a2b34c0bf5ec24367ba5869201c311251d72
vocsy Nikita <vocsyniki@gmail.com>
2023-03-02 10:34:26 +0530

    video add

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:holylazy/screen/home_screen.dart';
-import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,62 +25,71 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyApps extends StatelessWidget {
-  const MyApps({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> urls = [
-      "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
-      "http://techslides.com/demos/sample-videos/small.mp4",
-      "https://assets.mixkit.co/videos/preview/mixkit-young-women-jumping-at-the-concert-14116-large.mp4",
-      "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
-    ];
-
-    return MaterialApp(
-      home: PageView.builder(
-        itemBuilder: (ctx, index) => VideoScreen(url: urls[index]),
-        itemCount: urls.length,
-        scrollDirection: Axis.horizontal,
-      ),
-    );
-  }
-}
-
-class VideoScreen extends StatefulWidget {
-  final String url;
-  const VideoScreen({Key? key, required this.url}) : super(key: key);
-
-  @override
-  _VideoScreenState createState() => _VideoScreenState();
-}
-
-class _VideoScreenState extends State<VideoScreen> {
-  late VideoPlayerController controller;
-
-  void initController() async {
-    controller = VideoPlayerController.network(widget.url);
-    await controller.initialize();
-    controller.play();
-  }
-
-  @override
-  void initState() {
-    initController();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (controller.value.isInitialized) {
-      return const CircularProgressIndicator();
-    }
-    return SizedBox(
-      height: double.infinity,
-      child: AspectRatio(
-        aspectRatio: controller.value.aspectRatio,
-        child: VideoPlayer(controller),
-      ),
-    );
-  }
-}
+//
+// class MyApps extends StatefulWidget {
+//   const MyApps({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyApps> createState() => _MyAppsState();
+// }
+//
+// class _MyAppsState extends State<MyApps> {
+//   @override
+//   Widget build(BuildContext context) {
+//     List<String> urls = [
+//       "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+//       "http://techslides.com/demos/sample-videos/small.mp4",
+//       "https://assets.mixkit.co/videos/preview/mixkit-young-women-jumping-at-the-concert-14116-large.mp4",
+//       "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+//     ];
+//     return Scaffold(
+//       body: PageView.builder(
+//         itemBuilder: (ctx, index) => VideoScreen(
+//             url: urls[index].replaceFirst(
+//                 "https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4",
+//                 "http://techslides.com/demos/sample-videos/small.mp4",
+//                 0)),
+//         itemCount: urls.length,
+//         scrollDirection: Axis.horizontal,
+//       ),
+//     );
+//   }
+// }
+//
+// class VideoScreen extends StatefulWidget {
+//   final String url;
+//   const VideoScreen({Key? key, required this.url}) : super(key: key);
+//
+//   @override
+//   _VideoScreenState createState() => _VideoScreenState();
+// }
+//
+// class _VideoScreenState extends State<VideoScreen> {
+//   late VideoPlayerController controller;
+//
+//   void initController() async {
+//     controller = VideoPlayerController.network(widget.url);
+//     await controller.initialize();
+//     controller.play();
+//   }
+//
+//   @override
+//   void initState() {
+//     initController();
+//     super.initState();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     if (controller.value.isInitialized) {
+//       return const CircularProgressIndicator();
+//     }
+//     return SizedBox(
+//       height: double.infinity,
+//       child: AspectRatio(
+//         aspectRatio: controller.value.aspectRatio,
+//         child: VideoPlayer(controller),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/screen/multiplayer.dart
@@ -0,0 +1,278 @@
+// ignore_for_file: file_names
+
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
+
+class FlickMultiPlayer extends StatefulWidget {
+  const FlickMultiPlayer(
+      {Key? key,
+      required this.url,
+      this.image,
+      required this.isheaderVideo,
+      required this.flickMultiManager})
+      : super(key: key);
+
+  final String url;
+  final String? image;
+  final bool isheaderVideo;
+  final FlickMultiManager flickMultiManager;
+
+  @override
+  _FlickMultiPlayerState createState() => _FlickMultiPlayerState();
+}
+
+class _FlickMultiPlayerState extends State<FlickMultiPlayer> {
+  FlickManager? flickManager;
+
+  @override
+  void initState() {
+    flickManager = FlickManager(
+        videoPlayerController: VideoPlayerController.network(widget.url,
+            videoPlayerOptions: VideoPlayerOptions(
+              mixWithOthers: true,
+            )),
+        autoPlay: false,
+        onVideoEnd: () {
+          flickManager?.flickControlManager?.play();
+        });
+    widget.flickMultiManager.init(flickManager!);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.flickMultiManager.remove(flickManager!);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("This is value of Headers ${widget.isheaderVideo}");
+    return VisibilityDetector(
+      key: ObjectKey(flickManager),
+      onVisibilityChanged: (visiblityInfo) {
+        if (visiblityInfo.visibleFraction > 0.8) {
+          widget.flickMultiManager.play(flickManager);
+        } else {
+          widget.flickMultiManager.pause();
+        }
+      },
+      child: Container(
+        height: MediaQuery.of(context).size.width,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          border: Border.all(width: 0.0),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        foregroundDecoration: widget.isheaderVideo
+            ? BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Theme.of(context).scaffoldBackgroundColor,
+                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.0),
+                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.0),
+                    Theme.of(context).scaffoldBackgroundColor
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: const [0, 0.058, 0.95, 1],
+                ),
+              )
+            : null,
+        child: FlickVideoPlayer(
+          flickManager: flickManager!,
+          flickVideoWithControls: FlickVideoWithControls(
+            videoFit: BoxFit.fill,
+            playerLoadingFallback: Positioned.fill(
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    right: 10,
+                    top: 10,
+                    child: SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Shimmer.fromColors(
+                          baseColor: Theme.of(context)
+                              .unselectedWidgetColor
+                              .withOpacity(0.3),
+                          highlightColor: Colors.pink.withOpacity(0.2),
+                          child: Container(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.5),
+                          ),
+                        )),
+                  ),
+                ],
+              ),
+            ),
+            controls: FeedPlayerPortraitControls(
+              flickMultiManager: widget.flickMultiManager,
+              flickManager: flickManager,
+            ),
+          ),
+          flickVideoWithControlsFullscreen: const FlickVideoWithControls(
+            controls: FlickLandscapeControls(),
+            iconThemeData: IconThemeData(
+              size: 40,
+              color: Colors.white,
+            ),
+            textStyle: TextStyle(fontSize: 16, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FeedPlayerPortraitControls extends StatelessWidget {
+  const FeedPlayerPortraitControls(
+      {Key? key, this.flickMultiManager, this.flickManager})
+      : super(key: key);
+
+  final FlickMultiManager? flickMultiManager;
+  final FlickManager? flickManager;
+
+  @override
+  Widget build(BuildContext context) {
+    FlickDisplayManager displayManager =
+        Provider.of<FlickDisplayManager>(context);
+    return Container(
+      color: Colors.transparent,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          FlickAutoHideChild(
+            showIfVideoNotInitialized: false,
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                decoration: BoxDecoration(
+                  color: Colors.black38,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const FlickLeftDuration(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: FlickToggleSoundAction(
+              toggleMute: () {
+                flickMultiManager?.toggleMute();
+                displayManager.handleShowPlayerControls();
+              },
+              child: const FlickSeekVideoAction(
+                child: Center(child: FlickVideoBuffer()),
+              ),
+            ),
+          ),
+          FlickAutoHideChild(
+            autoHide: true,
+            showIfVideoNotInitialized: false,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: FlickSoundToggle(
+                    toggleMute: () => flickMultiManager?.toggleMute(),
+                    color: Colors.white,
+                  ),
+                ),
+                // FlickFullScreenToggle(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FlickMultiManager {
+  FlickMultiManager({
+    Key? key,
+    required this.mute,
+  });
+
+  bool mute = false;
+
+  final List<FlickManager> _flickManagers = [];
+  FlickManager? activeManager;
+
+  init(FlickManager flickManager) {
+    _flickManagers.add(flickManager);
+
+    if (mute) {
+      flickManager.flickControlManager?.mute();
+    } else {
+      flickManager.flickControlManager?.unmute();
+    }
+    if (_flickManagers.length == 1) {
+      // play(flickManager);
+    }
+  }
+
+  remove(FlickManager flickManager) {
+    if (activeManager == flickManager) {
+      activeManager = null;
+    }
+    flickManager.dispose();
+    _flickManagers.remove(flickManager);
+  }
+
+  togglePlay(FlickManager flickManager) {
+    if (activeManager?.flickVideoManager?.isPlaying == true &&
+        flickManager == activeManager) {
+      pause();
+    } else {
+      play(flickManager);
+    }
+  }
+
+  pause() {
+    activeManager?.flickControlManager?.pause();
+  }
+
+  play([FlickManager? flickManager]) {
+    if (flickManager != null) {
+      activeManager?.flickControlManager?.pause();
+      activeManager = flickManager;
+    }
+
+    if (mute) {
+      activeManager?.flickControlManager?.mute();
+    } else {
+      activeManager?.flickControlManager?.unmute();
+    }
+
+    activeManager?.flickControlManager?.play();
+  }
+
+  toggleMute() {
+    activeManager?.flickControlManager?.toggleMute();
+    mute = activeManager?.flickControlManager?.isMute ?? false;
+    if (mute) {
+      for (var manager in _flickManagers) {
+        manager.flickControlManager?.mute();
+      }
+    } else {
+      for (var manager in _flickManagers) {
+        manager.flickControlManager?.unmute();
+      }
+    }
+  }
+}
